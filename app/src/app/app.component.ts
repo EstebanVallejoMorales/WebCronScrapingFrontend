d94187dea783ff875235503143677df513b017d8
EstebanVallejoMorales <esteban.vallejomo@gmail.com>
2023-12-17 16:11:22 -0500

    Form for schedule and execute has been created

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -17,33 +17,56 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class AppComponent {
   title = 'app';
   
-  public url: string = '';
-  public cron: string = '';
+  public url1: string = '';
+  public cron1: string = '';
+  public url2: string = '';
+  public cron2: string = '';
 
-  // public webScrapingResultDto: WebScrapingResultDto;
-  // public webScrapingScheduleRequestDto: WebScrapingScheduleRequestDto
+  public webScrapingResultDto1: WebScrapingResultDto;
+  public webScrapingResultDto2: WebScrapingResultDto;
+  public webScrapingScheduleRequestDto: WebScrapingScheduleRequestDto
 
   public constructor(private api:WebCronScrapingService){
-    // this.webScrapingResultDto = {};
-    // this.webScrapingScheduleRequestDto = {
-    //   url: 'https://listado.mercadolibre.com.co/cervezas#D[A:cervezas]',
-    //   cron: '0 0/1 0 ? * * *'
-    // };
-    
-
-    // this.api.apiWebCronScrapingScheduleWebCronScrappingPost$Json({body: this.webScrapingScheduleRequestDto}).subscribe(
-    //   response => {
-    //     this.webScrapingResultDto = response;
-    //   }      
-    // )    
+    this.webScrapingResultDto1 = {};
+    this.webScrapingResultDto2 = {};
+    this.webScrapingScheduleRequestDto = {};    
   }
 
   schedule() {
     // Verifica que ambos campos no estén vacíos antes de hacer la llamada al servicio
-    if (this.url.trim() === '' || this.cron.trim() === '') {
+    if (this.url1.trim() === '' || this.cron1.trim() === '') {
       // Manejar el caso en el que los campos estén vacíos
       console.error('URL y Cron son campos requeridos.');
       return;
     }
+    else{
+      this.webScrapingScheduleRequestDto = {
+        url: this.url1,
+        cron: this.cron1
+      };
+      this.api.apiWebCronScrapingScheduleWebCronScrappingPost$Json({body: this.webScrapingScheduleRequestDto}).subscribe(
+        response => {
+          this.webScrapingResultDto1 = response;
+        }      
+      )    
+    }  
   }
+
+  execute() {
+    // Verifica que ambos campos no estén vacíos antes de hacer la llamada al servicio
+    if (this.url2.trim() === '') {
+      // Manejar el caso en el que los campos estén vacíos
+      alert('URL es un campo requerido.');
+      return;
+    }
+    else{
+      
+      this.api.apiWebCronScrapingGetWebCronScrappingGet$Json({url: this.url2}).subscribe(
+        response => {
+          this.webScrapingResultDto2 = response;
+        }      
+      )    
+    }  
+  }
+
 }
